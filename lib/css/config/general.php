10c786828bcd729eb6d2c784cd18ef4bf42cd800
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:31 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper ul, .editor-styles-wrapper ol' : '.sv100_sv_content .sv100_sv_content_wrapper > article ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid), .sv100_sv_content .sv100_sv_content_wrapper > article ol',
+		'.sv100_sv_content .sv100_sv_content_wrapper > article ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid), .sv100_sv_content .sv100_sv_content_wrapper > article ol',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
@@ -12,7 +12,7 @@
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper ul, .editor-styles-wrapper ol:not(.has-text-color)' : '.sv100_sv_content .sv100_sv_content_wrapper > article ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color), .sv100_sv_content .sv100_sv_content_wrapper > article ol:not(.has-text-color)',
+		'.sv100_sv_content .sv100_sv_content_wrapper > article ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color), .sv100_sv_content .sv100_sv_content_wrapper > article ol:not(.has-text-color)',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
