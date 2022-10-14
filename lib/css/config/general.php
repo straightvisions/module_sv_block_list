10e22dd81058d0c107d67dd3d09e8f7ea42fc6ab
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-10-14 09:52:25 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,9 +1,9 @@
 <?php
 	echo $_s->build_css(
-		'.wp-site-blocks ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid),
-		.wp-site-blocks ol,
-		.editor-styles-wrapper ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid),
-		.editor-styles-wrapper ol',
+		'.wp-site-blocks main ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid),
+		.wp-site-blocks main ol,
+		.editor-styles-wrapper main ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid),
+		.editor-styles-wrapper main ol',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
@@ -15,10 +15,10 @@
 	);
 
 	echo $_s->build_css(
-		'.wp-site-blocks ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color),
-		.wp-site-blocks ol:not(.has-text-color),
-		.editor-styles-wrapper ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color),
-		.editor-styles-wrapper ol:not(.has-text-color)',
+		'.wp-site-blocks main ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color),
+		.wp-site-blocks main ol:not(.has-text-color),
+		.editor-styles-wrapper main ul:not(.wp-block-navigation__container):not(.blocks-gallery-grid):not(.has-text-color),
+		.editor-styles-wrapper main ol:not(.has-text-color)',
 		array_merge(
 			$module->get_setting('text_color')->get_css_data(),
 		)
